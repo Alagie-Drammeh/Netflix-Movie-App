4f926f6a485e705499260b4dcd54e13540fa1c99
Marco Vignotto <marcovignotto81@gmail.com>
2021-07-01 13:48:30 +0200

    JsDocs ok

--- controllers/allCtrl.js
+++ controllers/allCtrl.js
@@ -2,11 +2,123 @@
  * @desc Ctrl for search route
  */
 
-// const url = require("url");
-// const querystring = require("querystring");
-
 const MovieSchema = require("../models/MovieSchema");
 const TvSchema = require("../models/TvSchema");
+
+/**
+ * @swagger
+ * /api/all:
+ *   get:
+ *     summary: Retrieve all data stored from MongoDB, query `?search=` optional returns searched items.
+ *     description: Default= returns obj with array `data` with all the movies and tv shows. Optional param `/?search=` (i.e. `/api/all?search=`) returns searched items.
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         description: i.e. `Luca` returns random item
+ *     responses:
+ *       200:
+ *         description: Movie, see the object examples bellow.
+ *         content:
+ *           Movie Object - application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                        adult:
+ *                          type: boolean
+ *                          description: Boolean Value
+ *                          example: false
+ *                        backdrop_path:
+ *                          type: string
+ *                          example: "/620hnMVLu6RSZW6a5rwO8gqpt0t.jpg"
+ *                        genre_ids:
+ *                          type: array
+ *                          example: [16, 35, 10751, 14]
+ *                        id:
+ *                          type: number
+ *                          example: 508943
+ *                        original_language:
+ *                          type: string
+ *                          example: "en"
+ *                        original_title:
+ *                          type: string
+ *                          example: "Original Title"
+ *                        overview:
+ *                          type: string
+ *                          example: "Bla bla bla..."
+ *                        popularity:
+ *                          type: number
+ *                          example: 508943.609
+ *                        poster_path:
+ *                          type: string
+ *                          example: "/jTswp6KyDYKtvC52GbHagrZbGvD.jpg"
+ *                        release_date:
+ *                          type: data
+ *                          example: "2021-06-17"
+ *                        title:
+ *                          type: string
+ *                          example: "Title"
+ *                        video:
+ *                          type: string
+ *                          example: false
+ *                        vote_average:
+ *                          type: number
+ *                          example: 9.8
+ *                        vote_count:
+ *                          type: number
+ *                          example: 2300
+ *           Tv Show Object- application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                        backdrop_path:
+ *                          type: string
+ *                          example: "/620hnMVLu6RSZW6a5rwO8gqpt0t.jpg"
+ *                        first_air_date:
+ *                          type: data
+ *                          example: "2021-06-17"
+ *                        genre_ids:
+ *                          type: array
+ *                          example: [16, 35, 10751, 14]
+ *                        id:
+ *                          type: number
+ *                          example: 508943
+ *                        name:
+ *                          type: string
+ *                        origin_country:
+ *                          type: array
+ *                          example: ["US"]
+ *                        original_language:
+ *                          type: string
+ *                          example: "en"
+ *                        original_name:
+ *                          type: string
+ *                          example: "Original Name"
+ *                        overview:
+ *                          type: string
+ *                          example: "Bla bla bla..."
+ *                        popularity:
+ *                          type: number
+ *                          example: 555.90
+ *                        poster_path:
+ *                          type: string
+ *                          example: "/jTswp6KyDYKtvC52GbHagrZbGvD.jpg"
+ *                        vote_average:
+ *                          type: number
+ *                          example: 55.9
+ *                        vote_count:
+ *                          type: number
+ *                          example: 2300
+ */
 
 exports.get = async (req, res, next) => {
   try {
--- controllers/dataCtrl.js
+++ controllers/dataCtrl.js
@@ -3,7 +3,6 @@
  */
 
 const axios = require("axios");
-const swaggerJSDoc = require("swagger-jsdoc");
 
 const axiosConfig = require("../functions/axiosConfig");
 
